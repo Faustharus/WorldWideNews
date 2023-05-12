c997299a836a75fb71d3ee55c0410c7fc3b44255
Dams920 <50359853+Dams920@users.noreply.github.com>
2023-05-12 17:41:20 +0200

    Initial Commit

---
+++ WorldWideNews/WorldWideNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldWideNewsApp.swift
+//  WorldWideNews
+//
+//  Created by Damien Chailloleau on 12/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorldWideNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
